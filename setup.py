d7e0c0425be170783b2c0a7976e0ab6f3b9bfaac
Yannis Bouhamou <61419311+Yanka13@users.noreply.github.com>
2022-02-21 17:31:49 +0000

    first commit + add script + remove useless files

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='bbquote',
       version="1.0",
-      description="Project Description",
+      description="Package for breaking bad API quote",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/bbquote-run'],
+      scripts=['scripts/quote'],
       zip_safe=False)
